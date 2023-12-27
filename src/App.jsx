7f3daf97706cc8f2f814b28b3310859a6bf8f3b5
Joao Fernandes <joao.fdias789@gmail.com>
2023-12-27 11:09:26 -0300

    Iserindo complete.todo e search

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import { useState } from 'react';
 
 import Todo from "./components/Todo";
 import TodoForm from './components/TodoForm';
+import Search from './components/Search';
 
 import "./App.css";
-
 
 function App() {
   const [todos, setTodos] = useState([
@@ -44,23 +44,37 @@ function App() {
 
   const removeTodo = (id) => {
     const newTodos = [...todos];
-    const filteredTodos = newTodos.filter(todo => 
+    const filteredTodos = newTodos.filter(todo =>
       todo.id !== id ? todo : null
-      );
-      setTodos(filteredTodos);
+    );
+    setTodos(filteredTodos);
+  };
+
+  const completeTodo = (id) => {
+    const newTodos = [...todos];
+    newTodos.map((todo) =>
+      todo.id === id ? (todo.isCompleted =
+        !todo.isCompleted) : todo);
+    setTodos(newTodos);
   };
 
   return (
     <div className="app">
       <h1>Lista de Tarefas</h1>
+      <Search />
       <div className="todo-list">
         {todos.map((todo) => (
-          <Todo key={todo.id} todo={todo} removeTodo={removeTodo}/>
+          <Todo 
+            key={todo.id}
+            todo={todo}
+            removeTodo={removeTodo}
+            completeTodo={completeTodo}
+          />
         ))}
       </div>
-      <TodoForm addTodo={addTodo}/>
+      <TodoForm addTodo={addTodo} />
     </div>
   );
-}
+  }
 
 export default App;
